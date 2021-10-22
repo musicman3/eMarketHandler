13764fda22636ec759d7433e818c5492f5cc9085
Alexander <36889207+musicman3@users.noreply.github.com>
2021-10-22 16:47:24 +0300

    Add files via upload

--- install.php
+++ install.php
@@ -207,7 +207,7 @@ function filesRemoving($path) {
  * GitHub Data
  * 
  * @param string $repo_init GitHub repo data
- * @return array GitHub latest release data
+ * @return string|bool GitHub latest release name
  */
 function gitHubData($repo_init) {
     $connect = curl_init();
@@ -232,6 +232,11 @@ function gitHubData($repo_init) {
         <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel = "stylesheet" integrity = "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin = "anonymous">
         <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin = "anonymous"></script>
         <script>
+            /**
+             * Ajax get
+             *
+             * @param url {String} (URL)
+             */
             function getUpdate(url) {
                 let xhr = new XMLHttpRequest();
                 xhr.open('GET', url, false);
@@ -240,7 +245,11 @@ function gitHubData($repo_init) {
                     success(xhr);
                 }
             }
-
+            /**
+             * Success
+             *
+             * @param xhr {Object} (xhr object)
+             */
             function success(xhr) {
                 var data = xhr.response;
                 var parse = JSON.parse(data);
